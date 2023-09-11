51834ac9a02839b0e0aa798904433b624c645ee3
Saiaravind92 <aravindreddy5076@gmail.com>
2023-09-11 16:29:56 -0500

    expense tracker

---
+++ src/components/Expenses.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses}/>
+      {/* {filteredExpenses.length === 0 ? ( Conditions can aslo be written as
+        <p> No Expenses Found</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )} */}
+    </Card>
+  );
+}
+
+export default Expenses;
